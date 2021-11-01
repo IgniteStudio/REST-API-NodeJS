e7c38ec47628ce90a05a0317ad071ea7f009fabc
David Agaybi <davidagaybi@gmail.com>
2021-11-01 16:17:53 -0400

    first commit

--- index.js
+++ index.js
@@ -1,8 +1,11 @@
-const express = require('express');
+const express = require("express");
+
 
 const app = express();
+app.use(express.json());
+
+app.use("/todos", require("./routes/todos"));
 
 app.listen(3000, () => {
-    console.log('Now listening on port 3000');
-    
-})
+  console.log("Now listening on port 3000");
+});
